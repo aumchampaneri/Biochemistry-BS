bc043f23ed8b719953fecf8e3b389c311b2eeda4
aumchampaneri <70304105+aumchampaneri@users.noreply.github.com>
2025-03-04 22:05:26 -0500

    Update Kidney data download script and related documentation

--- Thesis Project/Kidney/Kidney Data Download.py
+++ Thesis Project/Kidney/Kidney Data Download.py
@@ -5,11 +5,10 @@ Use this script to download the data from the cellxgene server
 The data will be downloaded to the data folder in the root directory of the project
 The data will be downloaded as a .h5ad file
 
-
-
+> https://cellxgene.cziscience.com/collections/854c0855-23ad-4362-8b77-6b1639e7a9fc
 '''
 
-cellxgene_census.download_census_data(
-    "xxx",
-    to_path=""
+cellxgene_census.download_source_h5ad(
+    "f95d8919-1f2a-405f-8776-bfecc0ab0f3f",
+    to_path="KidneyData.h5ad",
 )
--- Thesis Project/Miscellaneous/Link Dump.md
+++ Thesis Project/Miscellaneous/Link Dump.md
@@ -2,4 +2,5 @@
 > https://cellxgene.cziscience.com/collections/854c0855-23ad-4362-8b77-6b1639e7a9fc
 > 
 > Used to get Renal System data for _Homo sapiens_
-> 
+>
+> dataset_h5ad_path: f95d8919-1f2a-405f-8776-bfecc0ab0f3f.h5ad 
