ff5a218975b926b71046245a2601f7dd03b785c7
aumchampaneri <aum.champaneri@outlook.com>
2025-03-06 15:32:12 -0500

    Add notebooks and update documentation for mouse kidney data analysis

--- Thesis Project/Kidney/Kidney Data Download.py
+++ Thesis Project/Kidney/Kidney Data Download.py
@@ -5,10 +5,20 @@ Use this script to download the data from the cellxgene server
 The data will be downloaded to the data folder in the root directory of the project
 The data will be downloaded as a .h5ad file
 
+Human Kidney Data
 > https://cellxgene.cziscience.com/collections/854c0855-23ad-4362-8b77-6b1639e7a9fc
+
+Mouse Kidney Data
+> https://cellxgene.cziscience.com/collections/92fde064-2fb4-41f8-b85c-c6904000b859
+
 '''
 
+# cellxgene_census.download_source_h5ad(
+#     "f95d8919-1f2a-405f-8776-bfecc0ab0f3f",
+#     to_path="/Volumes/CHAMPANERI/Databases/KidneyData.h5ad",
+# )
+
 cellxgene_census.download_source_h5ad(
-    "f95d8919-1f2a-405f-8776-bfecc0ab0f3f",
-    to_path="/Volumes/CHAMPANERI/Databases/KidneyData.h5ad",
+    "42bb7f78-cef8-4b0d-9bba-50037d64d8c1",
+    to_path="/Volumes/CHAMPANERI/Databases/MouseKidneyData.h5ad",
 )
--- Thesis Project/Miscellaneous/Link Dump.md
+++ Thesis Project/Miscellaneous/Link Dump.md
@@ -4,3 +4,13 @@
 > Used to get Renal System data for _Homo sapiens_
 >
 > dataset_h5ad_path: f95d8919-1f2a-405f-8776-bfecc0ab0f3f.h5ad 
+
+
+> ### A comprehensive mouse kidney atlas enables rare cell population characterization and robust marker discovery
+> https://cellxgene.cziscience.com/collections/92fde064-2fb4-41f8-b85c-c6904000b859
+> 
+> Used to get Renal System data for _Mus musculus_
+> 
+> dataset_h5ad_path: 42bb7f78-cef8-4b0d-9bba-50037d64d8c1.h5ad
+ 
+
