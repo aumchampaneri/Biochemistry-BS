82853eab0acd5a37520551a40740fc8e8bdf3952
aumchampaneri <aum.champaneri@outlook.com>
2025-04-18 14:58:27 -0400

    Update dataset references and paths for Opiate Dependence project; add complement gene dictionary entry

--- Opiate Dependence/Data Download.py
+++ Opiate Dependence/Data Download.py
@@ -8,17 +8,18 @@ The data will be downloaded as a .h5ad file
 
 Run each of the download queries separately to download the data
 
-Human Kidney Data
-- Harmonized data from 4 datasets
-- Normal Single Cell = ~200k cells
+Human Dorsal Striatum in Opiate Use Disorder
+- 10x 3' v3 = ~100k cells
+- Caudate Nucleus and Putamen Tissue
+- Normal and Opiate Dependence Disease States
 
-> dataset_h5ad_path: f95d8919-1f2a-405f-8776-bfecc0ab0f3f
-> https://cellxgene.cziscience.com/collections/854c0855-23ad-4362-8b77-6b1639e7a9fc
+> dataset_h5ad_path: c893ddc3-f25b-45e2-8c9e-155918b4261c.h5ad
+> https://cellxgene.cziscience.com/collections/cec4ef8e-1e70-49a2-ae43-1e6bf1fd5978
 
 '''
 
-# cellxgene_census.download_source_h5ad(
-#     "f95d8919-1f2a-405f-8776-bfecc0ab0f3f",
-#     # to_path="/Volumes/CHAMPANERI/Databases/Human_Nor_Kidney_CellHint_scRNA.h5ad" # USB Location
-#     to_path="/Users/aumchampaneri/Databases/Human_Nor_Kidney_CellHint_scRNA.h5ad" # Disk Location
-# )
+cellxgene_census.download_source_h5ad(
+    "c893ddc3-f25b-45e2-8c9e-155918b4261c",
+    # to_path="/Users/aumchampaneri/Databases/Opiate Dependence/GSE233279_CxG.h5ad" # USB Location
+    to_path="/Users/aumchampaneri/Databases/Opiate Dependence/GSE233279_CxG.h5ad" # Disk Location
+)
--- Opiate Dependence/Preprocessing.py
+++ Opiate Dependence/Preprocessing.py
@@ -21,4 +21,4 @@ sc.pp.normalize_total(adata, target_sum=1e4)
 sc.pp.log1p(adata)
 
 # Save the processed data
-sc.write('/Users/aumchampaneri/Databases/Opiate Dependence/GSE233279_pp.h5ad', adata)
+sc.write('/Users/aumchampaneri/Databases/Opiate Dependence/GSE233279_CxG_pp.h5ad', adata)
